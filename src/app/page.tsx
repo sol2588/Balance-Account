f4524b1dc78fdb9b70d6687289f2ded6c975212f
sol2588 <dsk2588@gmail.com>
2024-08-25 23:51:53 +0900

    Feat: SignUp page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  id: string;
+  contents: string;
+}
+
 export default function Home() {
-  return <div></div>;
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push("/login");
+  };
+  const handleSignup = () => {
+    router.push("/signup");
+  };
+  return (
+    <div>
+      <p>BalanceAccount 회원인 경우</p>
+      <button onClick={handleLogin}>로그인</button>
+      <p>아직 BalanceAccount 회원이 아닌 경우</p>
+      <button onClick={handleSignup}>회원가입</button>
+    </div>
+  );
 }
